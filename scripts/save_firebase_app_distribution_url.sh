98f96830ccb89981f9fd4716e93eaf478fd0b0b1
Sergio Fierro <sergiofierro.perdomo@gmail.com>
2024-03-06 14:48:14 -0500

    Start E2E tests

---
+++ scripts/save_firebase_app_distribution_url.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if filename is provided as argument
+if [ $# -ne 1 ]; then
+    SELF=`basename $0`
+    echo "Usage: $SELF <FIREBASE_APP_DISTRIBUTION_OUTPUT_FILE>"
+    exit 1
+fi
+
+binary_download_uri=$(grep -o '"binaryDownloadUri":"[^"]*' "$1" | grep -o '[^"]*$')
+
+echo "Binary Download URI: $binary_download_uri"
+export ANDROID_APP_DOWNLOAD_URL=$binary_download_uri
+echo "$ANDROID_APP_DOWNLOAD_URL"
+
+TMP_FOLDER="tmp/workspace"
+mkdir -p $TMP_FOLDER
+echo "export ANDROID_APP_DOWNLOAD_URL=\"$ANDROID_APP_DOWNLOAD_URL\"" >> ${TMP_FOLDER}/env_vars
