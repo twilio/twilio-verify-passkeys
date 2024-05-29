d55f01b30337fd40fa5ef0ca8d274a7cae01f7b1
Sergio Fierro <sergiofierro.perdomo@gmail.com>
2024-05-29 14:55:49 -0500

    Modify script to support ios app url

--- scripts/e2e_tests/trigger_e2e_tests_pipeline.sh
+++ scripts/e2e_tests/trigger_e2e_tests_pipeline.sh
@@ -5,26 +5,38 @@ if [ $# -ne 5 ]; then
     exit 1
 fi
 
-ANDROID_APP_DOWNLOAD_URL=$1
+APP_DOWNLOAD_URL=$1
 TRIGGERING_WORKFLOW_ID=$2
 TRIGGERING_WORKFLOW_WAITING_JOB_NAME=$3
 TRIGGER_ANDROID_WORKFLOW=$4
 TRIGGER_IOS_WORKFLOW=$5
 
+DATA_PAYLOAD=$(jq -n \
+--arg trigger_android_workflow "$TRIGGER_ANDROID_WORKFLOW" \
+--arg trigger_ios_workflow "$TRIGGER_IOS_WORKFLOW" \
+--arg triggering_workflow_id "$TRIGGERING_WORKFLOW_ID" \
+--arg triggering_workflow_waiting_job_name "$TRIGGERING_WORKFLOW_WAITING_JOB_NAME" \
+'{
+    "branch": "sfierro/approve-job-in-workflow",
+    "parameters": {
+        "trigger-android-workflow": ($trigger_android_workflow | fromjson),
+        "trigger-ios-workflow": ($trigger_ios_workflow | fromjson),
+        "triggering-workflow-id": $triggering_workflow_id,
+        "triggering-workflow-waiting-job-name": $triggering_workflow_waiting_job_name
+    }
+}')
+
+if [ "$TRIGGER_IOS_WORKFLOW" == "true" ]; then
+    DATA_PAYLOAD=$(echo "$DATA_PAYLOAD" | jq --arg ios_app_url "$APP_DOWNLOAD_URL" '.parameters["ios-app-url"]=$ios_app_url')
+else
+    DATA_PAYLOAD=$(echo "$DATA_PAYLOAD" | jq --arg android_app_url "$APP_DOWNLOAD_URL" '.parameters["android-app-url"]=$android_app_url')
+fi
+
 TRIGGERED_PIPELINE=$(curl --request POST \
   --url https://circleci.com/api/v2/project/gh/twilio/twilio-verify-passkeys-tests/pipeline \
   --header "Circle-Token: $CIRCLE_TOKEN" \
   --header "Content-Type: application/json" \
-  --data '{
-    "branch":"sfierro/approve-job-in-workflow",
-    "parameters":{
-      "android-app-url":"'"$ANDROID_APP_DOWNLOAD_URL"'",
-      "trigger-android-workflow":'"$TRIGGER_ANDROID_WORKFLOW"',
-      "trigger-ios-workflow":'"$TRIGGER_IOS_WORKFLOW"',
-      "triggering-workflow-id":"'"$TRIGGERING_WORKFLOW_ID"'",
-      "triggering-workflow-waiting-job-name":"'"$TRIGGERING_WORKFLOW_WAITING_JOB_NAME"'"
-    }
-  }' | jq -r '.id')
+  --data "$DATA_PAYLOAD" | jq -r '.id')
 
 # Check if the pipeline creation was successful
 if [ -z "$TRIGGERED_PIPELINE" ]; then
